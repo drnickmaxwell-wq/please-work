21d62b3e946265c3f7a5acdfc81bbf7fd56c2952
nick maxwell <drnickmaxwell@gmail.com>
2025-12-05 09:10:41 +0000

    Align whitening preview hero with champagne layout

--- components/preview/treatments/whitening/WhiteningHero.tsx
+++ components/preview/treatments/whitening/WhiteningHero.tsx
@@ -1,6 +1,12 @@
 import { HeroEngine } from "@/components/champagne/hero/HeroEngine";
 import { whitening_hero } from "@/lib/champagne/hero-presets";
 
+import styles from "./whitening-preview.module.css";
+
 export function WhiteningHero() {
-  return <HeroEngine schema={whitening_hero} />;
+  return (
+    <div className={styles.heroBand}>
+      <HeroEngine schema={whitening_hero} />
+    </div>
+  );
 }
